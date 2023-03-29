afcda12dc2fda6216d882a2e2bf0abaa88f9e2fb
ForestTheKIng <67075855+ForestTheKIng@users.noreply.github.com>
2023-03-28 21:00:36 -0400

    remade CameraDrag.cs

--- valorant2d/Assets/CameraDrag.cs
+++ valorant2d/Assets/CameraDrag.cs
@@ -1,42 +1,48 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
 {
+    private Vector3 _origin;
+    private Vector3 _difference;
+    private Vector3 _resetCamera;
     public Camera cam;
-    private Vector3 _dragOrigin;
 
-    [SerializeField] private float zoomStep, minCamSize, maxCamSize;
+    private bool _drag = false;
+
     
-    private void Update()
+
+    private void Start()
     {
-        PanCamera();
+        _resetCamera = cam.transform.position;
     }
 
-    private void PanCamera()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
-        }
 
+    private void LateUpdate()
+    {
         if (Input.GetMouseButton(0))
         {
-            var difference = _dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-            
+            _difference = (cam.ScreenToWorldPoint(Input.mousePosition)) - cam.transform.position;
+            if(_drag == false)
+            {
+                _drag = true;
+                _origin = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+
         }
-    }
+        else
+        {
+            _drag = false;
+        }
 
+        if (_drag)
+        {
+            cam.transform.position = _origin - _difference * 0.5f;
+        }
 
-    public void ZoomIn()
-    {
-        var newSize = cam.orthographicSize + zoomStep;
-        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
-    }
+        if (Input.GetMouseButton(1))
+            cam.transform.position = _resetCamera;
 
-    public void ZoomOut()
-    {
-        var newSize = cam.orthographicSize + zoomStep;
-        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
     }
 }
